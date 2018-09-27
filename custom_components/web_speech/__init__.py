f296feb97a07dd1e8750c6d249d006d2cf02fec3
Zhong Jianxin <azuwis@gmail.com>
2018-09-27 11:49:41 +0800

    web_speech: pep8

--- custom_components/web_speech/__init__.py
+++ custom_components/web_speech/__init__.py
@@ -4,16 +4,17 @@ Provide functionality to transform speech into text using chromedriver.
 import asyncio
 import concurrent.futures
 import logging
-import subprocess
 import os.path
-
-_LOGGER = logging.getLogger(__name__)
+import subprocess
 
 REQUIREMENTS = ['selenium==3.14.1', 'xvfbwrapper==0.2.9']
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'web_speech'
 STATE = 'web_speech.web_speech'
 EVENT = 'speech_to_text'
+
 
 @asyncio.coroutine
 def async_setup(hass, config):
@@ -28,13 +29,15 @@ def async_setup(hass, config):
     subprocess.Popen(['pulseaudio'])
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--app=file://%s' % os.path.join(os.path.dirname(__file__), 'index.html'))
+    chrome_options.add_argument('--app=file://{}'.format(
+        os.path.join(os.path.dirname(__file__), 'index.html')))
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--single-process')
     chrome_options.add_argument('--start-fullscreen')
     chrome_options.add_argument('--use-fake-ui-for-media-stream')
-    chrome_options.add_argument('--user-data-dir=%s' % hass.config.path(DOMAIN))
+    chrome_options.add_argument('--user-data-dir={}'.format(
+        hass.config.path(DOMAIN)))
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
     listen = driver.find_element_by_id('listen')
@@ -52,17 +55,16 @@ def async_setup(hass, config):
     def async_listen(call):
         def wait_element(timeout, id, value):
             WebDriverWait(driver, timeout).until(
-                EC.text_to_be_present_in_element_value(
-                    (By.ID, id), value
-                )
-            )
+                EC.text_to_be_present_in_element_value((By.ID, id), value))
 
         listen.click()
 
         with concurrent.futures.ThreadPoolExecutor() as pool:
-            yield from loop.run_in_executor(pool, wait_element, 5, 'state', 'listening')
+            yield from loop.run_in_executor(
+                pool, wait_element, 5, 'state', 'listening')
             hass.states.async_set(STATE, 'listening', state_attrs)
-            yield from loop.run_in_executor(pool, wait_element, 60, 'state', 'idle')
+            yield from loop.run_in_executor(
+                pool, wait_element, 60, 'state', 'idle')
 
         text = driver.find_element_by_id('text').get_attribute('value')
         state_attrs['text'] = text
