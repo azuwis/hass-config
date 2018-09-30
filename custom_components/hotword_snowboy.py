5d701263fb60b0f75cc0e9b60fca514f6db59eb3
Zhong Jianxin <azuwis@gmail.com>
2018-09-30 19:43:44 +0800

    snowboy: Use concurrent.futures

--- custom_components/hotword_snowboy.py
+++ custom_components/hotword_snowboy.py
@@ -2,14 +2,14 @@
 Provide functionality to listen for a hot/wake word from snowboy.
 """
 import logging
+import concurrent.futures
 import os
 import asyncio
-import threading
 
 import voluptuous as vol
 
 from homeassistant.const import CONF_NAME, EVENT_HOMEASSISTANT_STOP
-from homeassistant.helpers import intent, config_validation as cv
+from homeassistant.helpers import config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -80,8 +80,6 @@ def async_setup(hass, config):
 
     assert os.path.exists(model), 'Model does not exist'
     detector = None
-    terminated = False
-    detected_event = threading.Event()
 
     state_attrs = {
         'friendly_name': 'Hotword',
@@ -90,42 +88,31 @@ def async_setup(hass, config):
 
     @asyncio.coroutine
     def async_listen(call):
-        nonlocal detected_event, terminated
         from snowboy import snowboydecoder
 
-        interrupted = False
-
-        def interrupt_callback():
-            nonlocal interrupted, terminated
-            return interrupted or terminated
-
-        def detect():
+        nonlocal detector
+        if detector == None:
             detector = snowboydecoder.HotwordDetector(
                 model, sensitivity=sensitivity, audio_gain=audio_gain)
 
-            detector.start(lambda: detected_event.set(),
-                           interrupt_check=interrupt_callback,
-                           sleep_time=0.03)
+        def detect():
+            def callback():
+                # Fire detected event
+                hass.bus.async_fire(EVENT_HOTWORD_DETECTED, {
+                    'name': name,       # name of the component
+                    'model': model      # model used
+                })
+                detector.terminate()
 
-            detector.terminate()
+            detector.start(callback)
 
         # Run detector in a separate thread
-        detected_event.clear()
-        thread = threading.Thread(target=detect, daemon=True)
         hass.states.async_set(OBJECT_SNOWBOY, STATE_LISTENING, state_attrs)
 
-        thread.start()
-        yield from asyncio.get_event_loop().run_in_executor(None, detected_event.wait)
-        interrupted = True
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            yield from asyncio.get_event_loop().run_in_executor(pool, detect)
 
-        if not terminated:
-            hass.states.async_set(OBJECT_SNOWBOY, STATE_IDLE, state_attrs)
-
-            # Fire detected event
-            hass.bus.async_fire(EVENT_HOTWORD_DETECTED, {
-                'name': name,       # name of the component
-                'model': model      # model used
-            })
+        hass.states.async_set(OBJECT_SNOWBOY, STATE_IDLE, state_attrs)
 
     hass.services.async_register(DOMAIN, SERVICE_LISTEN, async_listen)
     hass.states.async_set(OBJECT_SNOWBOY, STATE_IDLE, state_attrs)
@@ -133,9 +120,8 @@ def async_setup(hass, config):
     # Make sure snowboy terminates property when home assistant stops
     @asyncio.coroutine
     def async_terminate(event):
-        nonlocal terminated
-        terminated = True
-        detected_event.set()
+        if detector != None:
+            detector.terminate()
 
     hass.bus.async_listen(EVENT_HOMEASSISTANT_STOP, async_terminate)
 
