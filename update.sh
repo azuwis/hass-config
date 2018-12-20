e31836bf25dc601582557547ccf2051734762193
Zhong Jianxin <azuwis@gmail.com>
2018-12-20 10:21:42 +0800

    update.sh: Use github api to get latest release of mini-media-player

--- update.sh
+++ update.sh
@@ -11,8 +11,10 @@ download broadlink_climate_codes/gree.ini https://github.com/vpnmaster/homeassis
 download custom_components/climate/broadlink.py https://github.com/vpnmaster/homeassistant-custom-components/raw/master/custom_components/climate/broadlink.py
 download custom_components/climate/xiaomi_miio.py https://github.com/syssi/xiaomi_airconditioningcompanion/raw/develop/custom_components/climate/xiaomi_miio.py
 download custom_components/media_player/braviatv_psk.py https://github.com/gerard33/home-assistant/raw/master/braviatv_psk.py
-download www/mini-media-player.js https://github.com/kalkih/mini-media-player/releases/download/v0.9.2/mini-media-player-bundle.js
 download www/slider-entity-row.js https://github.com/thomasloven/lovelace-slider-entity-row/raw/master/slider-entity-row.js
 
 download themes/midnight.yaml https://github.com/maartenpaauw/home-assistant-community-themes/raw/master/midnight.yaml
 echo '  sidebar-icon-color: "#D0D0D0"' >> themes/midnight.yaml
+
+mini_media_player="$(curl --silent https://api.github.com/repos/kalkih/mini-media-player/releases/latest | awk -F'"' '/browser_download_url/ {print $4}')"
+download www/mini-media-player.js "$mini_media_player"
