ea05969447da023f5c1ac6aa5a38700f374b80b2
Zhong Jianxin <azuwis@gmail.com>
2019-03-04 17:45:03 +0800

    Add smartir code to update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ download() {
 
 download custom_components/smartir/climate.py https://github.com/smartHomeHub/SmartIR/raw/master/smartir/climate.py
 download custom_components/smartir/__init__.py https://github.com/smartHomeHub/SmartIR/raw/master/smartir/__init__.py
+download custom_components/smartir/codes/climate/1180.json https://github.com/smartHomeHub/SmartIR/raw/master/smartir/codes/climate/1180.json
 
 download custom_components/xiaomi_miio/climate.py https://github.com/syssi/xiaomi_airconditioningcompanion/raw/develop/custom_components/xiaomi_miio/climate.py
 download custom_components/xiaomi_miio/__init__.py https://github.com/syssi/xiaomi_airconditioningcompanion/raw/develop/custom_components/xiaomi_miio/__init__.py
