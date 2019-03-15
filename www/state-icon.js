4e48c6155af70c59466dc6d755925335b1e98d46
Zhong Jianxin <azuwis@gmail.com>
2019-03-16 00:41:15 +0800

    Refact custom state icon
    Using css style

--- www/state-icon.js
+++ www/state-icon.js
@@ -1,34 +1,23 @@
 class StateIcon extends Polymer.Element {
+  set hass(hass) {
+    this.content.hass = hass;
+  }
+
   setConfig(config) {
     if (!this.content) {
-      this.state = 'unavailable';
       this.content = document.createElement('hui-state-icon-element');
       this.appendChild(this.content);
     }
-    this.entity = config.entity;
     this.content.setConfig(config);
-  }
-
-  set hass(hass) {
-    this.content.hass = hass;
-    if (this.content.shadowRoot) {
-      let badge = this.content.shadowRoot.querySelector('state-badge');
-      if (badge) {
-        let state = hass.states[this.entity];
-        state = state ? state.state : 'unavailable';
-        if (this.state !== state) {
-          let icon = badge.shadowRoot.querySelector('ha-icon').shadowRoot.querySelector('svg');
-          if (state == 'off') {
-            icon.style.color = '';
-          } else if (state == 'unavailable') {
-            icon.style.color = 'var(--state-icon-unavailable-color)';
-          } else {
-            icon.style.color = 'var(--paper-item-icon-active-color)';
-          }
-        }
-        this.state = state;
-      }
-    }
+    this.content.updateComplete.then(() => {
+      this.content.shadowRoot.querySelector('state-badge').shadowRoot.querySelector('style').innerHTML += `
+ha-icon[data-domain="media_player"][data-state="on"],
+ha-icon[data-domain="climate"]:not([data-state="off"])
+{
+  color: var(--paper-item-icon-active-color);
+}
+`;
+    });
   }
 }
 
