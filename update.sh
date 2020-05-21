3e58d5a1611cc9c2e49dbfb1f89802ebfc9ace3f
Zhong Jianxin <azuwis@gmail.com>
2020-05-21 15:42:36 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -18,19 +18,19 @@ download_multi() {
 }
 
 download_multi custom_components/smartir https://github.com/smartHomeHub/SmartIR/raw/master/custom_components/smartir __init__.py climate.py controller.py fan.py manifest.json media_player.py services.yaml
-download custom_components/smartir/codes/climate/1180.json https://github.com/smartHomeHub/SmartIR/raw/master/codes/climate/1180.json
+# download custom_components/smartir/codes/climate/1180.json https://github.com/smartHomeHub/SmartIR/raw/master/codes/climate/1180.json
 mkdir -p a/custom_components b/custom_components
-rsync -a --exclude '__pycache__/' --exclude '**.orig' custom_components/smartir a/custom_components/
+rsync -a --exclude '__pycache__/' --exclude '**.orig' --exclude '**.rej' custom_components/smartir a/custom_components/
 patch -p1 -i patches/smartir.diff
-rsync -a --exclude '__pycache__/' --exclude '**.orig' custom_components/smartir b/custom_components/
+rsync -a --exclude '__pycache__/' --exclude '**.orig' --exclude '**.rej' custom_components/smartir b/custom_components/
 diff -Nur a b > patches/smartir.diff
-rm -r a b
+#rm -r a b
 
 download_multi custom_components/xiaomi_miio_airconditioningcompanion https://github.com/syssi/xiaomi_airconditioningcompanion/raw/develop/custom_components/xiaomi_miio_airconditioningcompanion __init__.py climate.py manifest.json
 
 download_multi custom_components/braviatv_psk https://github.com/custom-components/media_player.braviatv_psk/raw/master/custom_components/braviatv_psk __init__.py manifest.json media_player.py
 
-download www/slider-entity-row.js https://github.com/thomasloven/lovelace-slider-entity-row/raw/master/slider-entity-row.js
+# download www/slider-entity-row.js https://github.com/thomasloven/lovelace-slider-entity-row/raw/master/slider-entity-row.js
 
 download themes/midnight.yaml https://github.com/maartenpaauw/home-assistant-community-themes/raw/master/midnight.yaml
 echo '  sidebar-icon-color: "#D0D0D0"' >> themes/midnight.yaml
